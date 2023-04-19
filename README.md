4678ad13f7a2c3d4be65e109fee4a2dc1691b639
ml17950 <ml17950@gmail.com>
2023-04-19 13:45:16 +0200

    2023-04-19: Bugfix (TypeError: array_reverse / changed constructor)

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # dokuwiki-plugin-lastpages
+
 LastPages Plugin for Dokuwiki
 
 For more information visit https://www.dokuwiki.org/plugin:lastpages
+
+=== Change Log ===
+
+  * **2021-01-01**: Initial release
+  * **2021-01-03**: Downloadlink updated
+  * **2021-01-28**: Bugfix
+  * **2021-05-29**: Added ~~LASTPAGES~~ as keyword and options in the admin backend.
+  * **2021-06-16**: Don't display deleted pages.
+  * **2023-04-19**: Bugfix (TypeError: array_reverse / changed constructor)
--- syntax.php
+++ syntax.php
@@ -1,5 +1,4 @@
-<?php
-//	Last Change 2021-01-01
+<?php	//	Last Change 2023-04-19
 
 if(!defined('DOKU_INC')) {
     define ('DOKU_INC', realpath(dirname(__FILE__).'/../../').'/');
@@ -18,12 +17,11 @@ require_once (DOKU_INC.'inc/pageutils.php');
 class syntax_plugin_lastpages extends DokuWiki_Syntax_Plugin {
 	var $indexdir = '';
 	var $pages = array();
-	var $titles = array();
 
     /**
      * Constructor
      */
-    function syntax_plugin_lastpages() {
+    function __construct() {
     	global $conf;
 
 		$this->indexdir = $conf['indexdir'];
@@ -53,6 +51,7 @@ class syntax_plugin_lastpages extends DokuWiki_Syntax_Plugin {
      * Register supported keywords
      */
     function connectTo($mode) {
+    	$this->Lexer->addSpecialPattern('~~LASTPAGES~~', $mode, 'plugin_lastpages');
         $this->Lexer->addSpecialPattern('~~LAST10~~', $mode, 'plugin_lastpages');
         $this->Lexer->addSpecialPattern('~~LAST5~~', $mode, 'plugin_lastpages');
         $this->Lexer->addSpecialPattern('~~LIST10~~', $mode, 'plugin_lastpages');
@@ -63,7 +62,7 @@ class syntax_plugin_lastpages extends DokuWiki_Syntax_Plugin {
      * Handle the match
      */
     function handle($match, $state, $pos, Doku_Handler $handler) {
-    	return preg_replace("%~~LAST10~~%", "\\2", $match);
+    	return $match;
     }
 
     /**
@@ -75,13 +74,18 @@ class syntax_plugin_lastpages extends DokuWiki_Syntax_Plugin {
 			case '~~LIST5~~':	$number_of_pages = 5;	$type_of_list = 'list'; break;
 			case '~~LIST10~~':	$number_of_pages = 10;	$type_of_list = 'list'; break;
 			case '~~LAST5~~':	$number_of_pages = 5;	$type_of_list = 'inline'; break;
-			default:
 			case '~~LAST10~~':	$number_of_pages = 10;	$type_of_list = 'inline'; break;
+			default:
+			case '~~LASTPAGES~~': $number_of_pages = $this->getConf('numpages'); $type_of_list = $this->getConf('displist'); break;
 		}
 
+		$separator = $this->getConf('listsep');
+		if (empty($separator))
+			$separator = '&bull;';
+
     	$this->_getLatestPages($number_of_pages);			// get list of latest pages
-    	$data = $this->_formatLatestPages($type_of_list);	// format list of pages
-    	$renderer->doc .= $data;							// append data to the output stream
+    	$docdata = $this->_formatLatestPages($type_of_list, $separator);	// format list of pages
+    	$renderer->doc .= $docdata;							// append data to the output stream
     	return true;
     }
 
@@ -92,27 +96,51 @@ class syntax_plugin_lastpages extends DokuWiki_Syntax_Plugin {
 	function _getLatestPages($number_of_pages) {
 		$index_links_file = $this->indexdir.'/page.idx';
 		$index_title_file = $this->indexdir.'/title.idx';
+		$index_pword_file = $this->indexdir.'/pageword.idx';
 
 		$index_links = file($index_links_file);
 		$index_title = file($index_title_file);
+		$index_pword = file($index_pword_file);
 
-		$this->pages  = array_reverse(array_slice($index_links, ($number_of_pages * -1), $number_of_pages));
-		$this->titles = array_reverse(array_slice($index_title, ($number_of_pages * -1), $number_of_pages));
+		if (!is_array($index_links))
+			return;
+		if (!is_array($index_title))
+			return;
+		if (!is_array($index_pword))
+			return;
+
+		$pages     = array_reverse($index_links);
+		$titles    = array_reverse($index_title);
+		$pagewords = array_reverse($index_pword);
+		$cnt       = 0;
+
+		foreach ($pages as $id => $page) {
+			if (strlen($pagewords[$id]) > 2) {
+				$key = trim($page);
+				$title = trim($titles[$id]);
+				if (!empty($title))
+					$this->pages[$key] = $title;
+				else
+					$this->pages[$key] = $key;
+				$cnt++;
+				if ($cnt == $number_of_pages)
+					break;
+			}
+		}
 	}
 
-	function _formatLatestPages($type_of_list) {
+	function _formatLatestPages($type_of_list, $separator) {
 		$ret = '';
 
 		if (is_array($this->pages) && (count($this->pages) > 0)) {
 			if ($type_of_list == 'list')
 				$ret .= '<ul>';
-			foreach ($this->pages as $id => $page) {
-				$title = $this->titles[$id];
+			foreach ($this->pages as $key => $title) {
 				if ($type_of_list == 'list')
-					$ret .= '<li>'.html_wikilink(trim($page), $title).'</li>';
+					$ret .= '<li>'.html_wikilink($key, $title).'</li>';
 				else
-					$ret .= $sep.html_wikilink(trim($page), $title);
-				$sep = ' &bull; ';
+					$ret .= $sep.html_wikilink($key, $title);
+				$sep = ' '.$separator.' ';
 			}
 			if ($type_of_list == 'list')
 				$ret .= '</ul>';
